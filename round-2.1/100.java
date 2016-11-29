17625ad394e200d10874546f6caa883c3fa153f7
kophy <wenjietao179@gmail.com>
2016-11-28 22:39:05 -0800

    2016.11.28

---
+++ round-2.1/100.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        else if (p == null ^ q == null)
+            return false;
+        else
+            return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
