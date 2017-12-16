383b5c471582b3055f3d99651c5a98d9a359122e
kophy <kophy@protonmail.com>
2017-12-16 02:01:03 -0800

    remove java part

--- Java/021-040/026.java
+++
@@ -1,9 +0,0 @@
-public class Solution {
-    public int removeDuplicates(int[] nums) {
-        int len = 0;
-        for (int num : nums)
-            if (len == 0 || nums[len - 1] != num)
-                nums[len++] = num;
-        return len;
-    }
-}
--- Java/021-040/027.java
+++
@@ -1,9 +0,0 @@
-public class Solution {
-    public int removeElement(int[] nums, int val) {
-        int len = 0;
-        for (int num : nums)
-            if (num != val)
-                nums[len++] = num;
-        return len;
-    }
-}
