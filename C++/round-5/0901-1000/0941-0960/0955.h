e90dae174003b5205ab901cf86139c067ff474f8
kophy <kophy@protonmail.com>
2018-12-16 18:26:34 -0800

    2018.12.16

---
+++ C++/round-5/0901-1000/0941-0960/0955.h
@@ -0,0 +1,30 @@
+class Solution {
+ public:
+  int minDeletionSize(vector<string> &A) {
+    int N = A[0].size();
+    int deletion = 0;
+    vector<bool> sorted((int)A.size() - 1, false);
+    for (int j = 0; j < N; ++j) {
+      if (keepSorted(A, sorted, j)) {
+        for (int i = 0; i < (int)A.size() - 1; ++i) {
+          if (A[i][j] < A[i + 1][j]) {
+            sorted[i] = true;
+          }
+        }
+      } else {
+        ++deletion;
+      }
+    }
+    return deletion;
+  }
+
+ private:
+  bool keepSorted(vector<string> &A, vector<bool> &sorted, int j) {
+    for (int i = 0; i < (int)A.size() - 1; ++i) {
+      if (!sorted[i] && (A[i][j] > A[i + 1][j])) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
---
+++ C++/round-5/0901-1000/0941-0960/0957.h
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> prisonAfterNDays(vector<int> &cells, int N) {
+        vector<vector<int>> results;
+        unordered_map<string, int> pattern_to_day;
+        pattern_to_day[makePattern(cells)] = N;
+
+        while (N > 0) {
+            simulateOneDay(cells);
+            --N;
+            string pattern = makePattern(cells);
+            if (pattern_to_day.count(pattern)) {
+                int T = pattern_to_day[pattern] - N;
+                N %= T;
+            } else {
+                results.push_back(cells);
+                pattern_to_day[pattern] = N;
+            }
+        }
+        return cells;
+    }
+
+private:
+    string makePattern(vector<int> &cells) {
+        stringstream ss;
+        for (int cell : cells) {
+            ss << cell;
+        }
+        return ss.str();
+    }
+
+    void simulateOneDay(vector<int> &cells) {
+        for (int i = 1; i < (int)cells.size() - 1; ++i) {
+            if ((cells[i - 1] & 1) == (cells[i + 1] & 1)) {
+                cells[i] |= 2;
+            }
+        }
+        for (int i = 0; i < cells.size(); ++i) {
+            cells[i] >>= 1;
+        }
+    }
+};
