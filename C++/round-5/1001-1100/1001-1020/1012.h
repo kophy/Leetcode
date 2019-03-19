01c7cf6ce076155a977be1aadecfc6a45947bc2a
kophy <kophy@protonmail.com>
2019-03-18 22:28:41 -0700

    2019.03.18

---
+++ C++/round-5/1001-1100/1001-1020/1012.h
@@ -0,0 +1,19 @@
+class Solution {
+ public:
+  int bitwiseComplement(int N) {
+    if (N == 0) {
+      return 1;
+    }
+    bitset<32> b(N);
+    bool flip = false;
+    for (int i = 31; i >= 0; --i) {
+      if (!flip && b[i]) {
+        flip = true;
+      }
+      if (flip) {
+        b.flip(i);
+      }
+    }
+    return b.to_ulong();
+  }
+};
---
+++ C++/round-5/1001-1100/1001-1020/1013.h
@@ -0,0 +1,18 @@
+class Solution {
+ public:
+  int numPairsDivisibleBy60(vector<int>& time) {
+    unordered_map<int, int> m;
+    for (int t : time) {
+      ++m[t % 60];
+    }
+    int result = 0;
+    for (const auto& p : m) {
+      if (p.first == 0 || p.first == 30) {
+        result += p.second * (p.second - 1);
+      } else if (m.count(60 - p.first)) {
+        result += p.second * m[60 - p.first];
+      }
+    }
+    return result / 2;
+  }
+};
