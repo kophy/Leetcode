90616a1abeab7cf33133f1cd3b011842d10b7dce
kophy <kophy@protonmail.com>
2019-04-21 16:59:59 -0700

    2019.04.21

---
+++ C++/round-5/1001-1100/1001-1020/1007.h
@@ -0,0 +1,30 @@
+class Solution {
+ public:
+  int minDominoRotations(vector<int> &A, vector<int> &B) {
+    // 1. check if there is a value shared by all dominoes.
+    unordered_set<int> candidates = {A[0], B[0]};
+    for (int i = 0; i < A.size(); ++i) {
+      if (A[i] != A[0] && B[i] != A[0]) {
+        candidates.erase(A[0]);
+      }
+      if (A[i] != B[0] && B[i] != B[0]) {
+        candidates.erase(B[0]);
+      }
+    }
+    if (candidates.empty()) {
+      return -1;
+    }
+    int candidate = *candidates.begin();
+    // 2. compare the cost between flipping up and down.
+    int count_top = 0, count_bottom = 0;
+    for (int i = 0; i < A.size(); ++i) {
+      if (A[i] != candidate) {
+        ++count_top;
+      }
+      if (B[i] != candidate) {
+        ++count_bottom;
+      }
+    }
+    return min(count_top, count_bottom);
+  }
+};
---
+++ C++/round-5/1001-1100/1021-1040/1032.h
@@ -0,0 +1,75 @@
+class StreamChecker {
+ public:
+  StreamChecker(vector<string> &words) {
+    root = new SuffixTreeNode();
+    max_length = 0;
+    for (string &word : words) {
+      insertWord(root, word);
+      max_length = max(max_length, (int)word.size());
+    }
+  }
+
+  bool query(char letter) {
+    buffer.push_back(letter);
+    if (buffer.size() > max_length) {
+      buffer = buffer.substr(1);
+    }
+    return findWord(root, buffer);
+  }
+
+ private:
+  typedef struct SuffixTreeNode {
+    bool is_leaf;
+    unordered_map<char, SuffixTreeNode *> children;
+    SuffixTreeNode() : is_leaf(false) {}
+  };
+
+  void insertWord(SuffixTreeNode *root, string &word) {
+    string reversed_word = word;
+    reverse(reversed_word.begin(), reversed_word.end());
+    insertWordHelper(root, reversed_word, 0);
+  }
+
+  void insertWordHelper(SuffixTreeNode *root, string &reversed_word,
+                        int index) {
+    if (index == reversed_word.size()) {
+      root->is_leaf = true;
+      return;
+    }
+    char c = reversed_word[index];
+    if (!root->children.count(c)) {
+      root->children[c] = new SuffixTreeNode();
+    }
+    insertWordHelper(root->children[c], reversed_word, index + 1);
+  }
+
+  bool findWord(SuffixTreeNode *root, string &word) {
+    string reversed_word = word;
+    reverse(reversed_word.begin(), reversed_word.end());
+    return findWordHelper(root, reversed_word, 0);
+  }
+
+  bool findWordHelper(SuffixTreeNode *root, string &reversed_word, int index) {
+    if (root->is_leaf) {
+      return true;
+    }
+    if (index == reversed_word.size()) {
+      return root->is_leaf;
+    }
+    char c = reversed_word[index];
+    if (root->children.count(c)) {
+      return findWordHelper(root->children[c], reversed_word, index + 1);
+    }
+    return false;
+  }
+
+  SuffixTreeNode *root;
+  int max_length;
+  string buffer;
+};
+
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * StreamChecker* obj = new StreamChecker(words);
+ * bool param_1 = obj->query(letter);
+ */
