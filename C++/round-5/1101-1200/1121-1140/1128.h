19f27e2c3c85ab9c0554fcc5950550d8b8744b58
kophy <kophy@protonmail.com>
2019-12-15 16:56:34 -0800

    previous work

---
+++ C++/round-5/1101-1200/1121-1140/1128.h
@@ -0,0 +1,16 @@
+class Solution {
+ public:
+  int numEquivDominoPairs(vector<vector<int>> &dominoes) {
+    unordered_map<string, int> count;
+    for (const auto &d : dominoes) {
+      stringstream ss;
+      ss << std::min(d[0], d[1]) << ":" << std::max(d[0], d[1]);
+      ++count[ss.str()];
+    }
+    int result = 0;
+    for (const auto &p : count) {
+      result += p.second * (p.second - 1) / 2;
+    }
+    return result;
+  }
+};
