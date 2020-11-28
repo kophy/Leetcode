57fa3d44eb3d5c2aaf0f1392b56039af198ff756
kophy <kophy@protonmail.com>
2020-11-27 17:03:35 -0800

    2020-11-27

---
+++ C++/round-5/0901-1000/0961-0980/0973.h
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        auto cmp = [](const pair<long, int>& a, const pair<long, int>& b)->bool {
+            return a.first < b.first;
+        };
+        std::priority_queue<pair<long, int>, std::vector<pair<long, int>>, decltype(cmp)> k_closest_points(cmp);
+
+        for (int i = 0; i < points.size(); ++i) {
+            const auto& point = points[i];
+            long distance = pow(point[0], 2) + pow(point[1], 2);
+            if (k_closest_points.size() < K) {
+                k_closest_points.push(std::make_pair(distance, i));
+            } else if (distance < k_closest_points.top().first) {
+                k_closest_points.pop();
+                k_closest_points.push(std::make_pair(distance, i));
+            }
+        }
+        vector<vector<int>> result;
+        for (int i = 0; i < K; ++i) {
+            int index = k_closest_points.top().second;
+            k_closest_points.pop();
+            result.push_back(points[index]);
+        }
+        return result;
+    }
+};
---
+++ C++/round-5/1101-1200/1161-1180/1167.h
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int connectSticks(vector<int>& sticks) {
+        std::priority_queue<int, std::vector<int>, std::greater<int>> candidates;
+        for (int stick : sticks) {
+            candidates.push(stick);
+        }
+        int cost = 0;
+        while (candidates.size() > 1) {
+            int s1 = candidates.top();
+            candidates.pop();
+            int s2 = candidates.top();
+            candidates.pop();
+
+            candidates.push(s1 + s2);
+            cost += (s1 + s2);
+        }
+        return cost;
+    }
+};
