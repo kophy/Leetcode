179a580c830cee722b5806c4f855b0b3f7c0508f
kophy <kophy@protonmail.com>
2020-12-09 19:24:21 -0800

    2020-12-09

---
+++ C++/round-5/1001-1100/1021-1040/1038.h
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* bstToGst(TreeNode* root) {
+        int sum = 0;
+        stack<TreeNode *> data;
+        for (TreeNode *node = root; node != nullptr; node = node->right) {
+            data.push(node);
+        }
+        
+        while (!data.empty()) {
+            TreeNode *node = data.top();
+            data.pop();
+            
+            int temp = node->val;
+            node->val += sum;
+            sum += temp;
+            
+            node = node->left;
+            for (; node != nullptr; node = node->right) {
+                data.push(node);
+            }
+        }
+        
+        return root;
+    }
+};
---
+++ C++/round-5/1001-1100/1041-1060/1043.h
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        if (arr.empty()) {
+            return 0;
+        }
+        int N = arr.size();
+        vector<int> memo(N + 1, 0);
+        return helper(arr, k, 0, memo);
+    }
+    
+private:
+    int helper(vector<int> &arr, int k, int begin, vector<int> &memo) {
+        if (memo[begin] > 0) {
+            return memo[begin];
+        }
+        if (begin >= arr.size()) {
+            return 0;
+        }
+        
+        int curr_max = memo[begin];
+        for (int i = begin; i < arr.size() && i < begin + k; ++i) {
+            curr_max = max(curr_max, arr[i]);
+            memo[begin] = max(memo[begin], curr_max * (i - begin + 1) + helper(arr, k, i + 1, memo));
+        }
+        return memo[begin];
+    }
+};
