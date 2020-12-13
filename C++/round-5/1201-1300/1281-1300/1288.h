fce0a484c97ec33675cc53acdd6f5e80315702e7
kophy <kophy@protonmail.com>
2020-12-13 15:07:11 -0800

    2020-12-13

---
+++ C++/round-5/1201-1300/1281-1300/1288.h
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const vector<int> &a, const vector<int> &b)->bool {
+            return (a[0] < b[0]) || (a[0] == b[0] && a[1] > b[1]);
+        });
+        
+        int last = 0;
+        int result = 1;
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[last][1] >= intervals[i][1]) {
+                continue;
+            } else {
+                last = i;
+                ++result;
+            }
+        }
+        return result;
+    }
+};
---
+++ C++/round-5/1201-1300/1281-1300/1291.h
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        vector<int> result;
+        for (int i = 1; i <= 9; ++i) {
+            backtracking(result, low, high, i);
+        }
+        sort(result.begin(), result.end());
+        return result;
+    }
+    
+private:
+    void backtracking(vector<int> &result, int low, int high, int current) {
+        if (current > high) {
+            return;
+        }
+        if (current >= low && current <= high) {
+            result.push_back(current);
+        }
+        
+        int last_digit = current % 10;
+        if (last_digit < 9) {
+            backtracking(result, low, high, current * 10 + last_digit + 1);
+        }
+    }
+};
