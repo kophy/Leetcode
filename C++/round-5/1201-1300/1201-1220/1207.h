2ffb7110a422d4d6542c7852597edba781c0c8b2
kophy <kophy@protonmail.com>
2020-12-15 15:46:12 -0800

    2020-12-15

---
+++ C++/round-5/1201-1300/1201-1220/1207.h
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> occurrences;
+        for (int x : arr) {
+            ++occurrences[x];
+        }
+        
+        unordered_set<int> unique_occurrences;
+        for (const auto& p : occurrences) {
+            if (unique_occurrences.count(p.second)) {
+                return false;
+            }
+            unique_occurrences.insert(p.second);
+        }
+        return true;
+    }
+};
---
+++ C++/round-5/1201-1300/1201-1220/1208.h
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int equalSubstring(string s, string t, int maxCost) {
+        int currentCost = 0;
+        int left = 0, result = 0;
+        
+        for (int i = 0; i < s.size(); ++i) {
+            currentCost += abs(s[i] - t[i]);
+            while (left <= i && currentCost > maxCost) {
+                currentCost -= abs(s[left] - t[left]);
+                ++left;
+            }
+            result = max(result, i - left + 1);
+        }
+        return result;
+    }
+};
